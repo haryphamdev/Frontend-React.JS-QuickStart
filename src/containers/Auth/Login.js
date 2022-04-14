e829bb913ee4d0f14bbdb1b49769fbb5e09c5539
natsupy <15712687+natsupy@users.noreply.github.com>
2022-04-14 08:53:25 +0700

    update

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -103,6 +103,9 @@ class Login extends Component {
                         <div className='col-12'>
                             <span className='forgot-password'>Forgot your password?</span>
                         </div>
+                        <div className='col-12'>
+                            <span className='register'>Don't have account, Register!</span>
+                        </div>
                         <div className='col-12 text-center mt-3'>
                             <span className='text-other-login'>Or Login with</span>
                         </div>
